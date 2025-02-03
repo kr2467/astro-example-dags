21cc96988d5367eb94389628e424780ef363d2d4
kr2467 <104669142+kr2467@users.noreply.github.com>
2025-02-03 22:53:56 +0530

    Update branching.py

--- dags/branching.py
+++ dags/branching.py
@@ -1,47 +1,49 @@
 from airflow import DAG
 from datetime import datetime
 from airflow.operators.python_operator import BranchPythonOperator
-from airflow.opertors.dummy_operators import DummyOperator
-
+from airflow.operators.dummy_operator import DummyOperator  # Corrected import
 
 def choose_branch(**kwargs):
-    day_of_week=datetime.now().weekday()
+    day_of_week = datetime.now().weekday()
     
-    if day_of_week<5:
+    if day_of_week < 5:
         return 'branch_1'
     else:
         return 'branch_2'
 
 with DAG('simple_dag',
-         start_date=datetime(2025,3,2),
+         start_date=datetime(2025, 3, 2),
          title='a simple dag run',
          schedule='@daily',
          catchup=False
          ) as dag:
-    start_task=DummyOperator(
+
+    start_task = DummyOperator(
         task_id='start',
         dag=dag
-        )
-    branching=BranchPythonOperator(
+    )
+    
+    branching = BranchPythonOperator(
         task_id='branching',
         python_callable=choose_branch,
         provide_context=True
-        )
-    branch_1=DummyOperator(
+    )
+    
+    branch_1 = DummyOperator(
         task_id='branch_1',
         dag=dag
-        )
-    branch_2=DummyOperator(
+    )
+    
+    branch_2 = DummyOperator(
         task_id='branch_2',
         dag=dag
-        )
-    end_task=DummyOperator(
+    )
+    
+    end_task = DummyOperator(
         task_id='end',
         dag=dag
-        )
-    start_task>>branching>>[branch_1,branch_2]
-    branch_1>>end_task
-    branch_2>>end_task
+    )
     
-    
-    
+    start_task >> branching >> [branch_1, branch_2]
+    branch_1 >> end_task
+    branch_2 >> end_task
