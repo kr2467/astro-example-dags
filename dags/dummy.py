aca17f9caca2ec63435924e3238f1205bd3c537b
kr2467 <104669142+kr2467@users.noreply.github.com>
2025-02-03 22:24:05 +0530

    Create dummy.py

---
+++ dags/dummy.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from datetime import datetime
+
+with DAG('dummy_operator',
+        start_date=datetime(2025, 3, 2),
+        description='a dummy operator',
+        tags=['data'],
+        schedule='@daily',
+        catchup=False
+        ) as dag:
+
+    start_task = DummyOperator(
+        task_id='start',  # Corrected parameter name here
+        dag=dag
+    )
+    
+    end_operator = DummyOperator(
+        task_id='end',  # Corrected parameter name here
+        dag=dag
+    )
+
+    start_task >> end_operator
