ff56878041a7f4005e75617e8b0804a661f892d3
kr2467 <104669142+kr2467@users.noreply.github.com>
2025-02-04 16:07:58 +0530

    Update task_groups.py

--- dags/task_groups.py
+++ dags/task_groups.py
@@ -2,22 +2,28 @@ from airflow import DAG
 from datetime import datetime
 from airflow.dummy_operator import DummyOperator
 from airflow.utils.task_group import TaskGroup
+
 with DAG(
         'task_group',
         schedule='@once',
         start_date=datetime(2025,4,2),
         catchup=False
         ) as dag:
-    start=DummyOperator(
+    start = DummyOperator(
         task_id='starting',
-        )
-    with TaskGroup("extract_task") as extract_tasks:
-        extract_data=DummyOperator(task_id='extract_data')
-        extract_data1=DummyOperator(task_id='extract_data1')
-    with TaskGroup("transform_tasks") as transform_groups:
-        transform_data=DummyOperator(task_id='transform_data')
-        transform_data1=DummyOperator(task_id='tansform_data2')
-        
-    end=DummyOperator(task_id='end')
+    )
     
-    start>>extract_tasks>>transform_groups>>end
+    # Task Group for extract tasks
+    with TaskGroup("extract_task") as extract_tasks:
+        extract_data = DummyOperator(task_id='extract_data')
+        extract_data1 = DummyOperator(task_id='extract_data1')
+    
+    # Task Group for transform tasks
+    with TaskGroup("transform_tasks") as transform_groups:
+        transform_data = DummyOperator(task_id='transform_data')
+        transform_data1 = DummyOperator(task_id='transform_data2')  
+    
+    end = DummyOperator(task_id='end')
+    
+    # Defining the task dependencies
+    start >> extract_tasks >> transform_groups >> end
