6eed222faf7b7eab4f445b7a724475fd36af1df0
kr2467 <104669142+kr2467@users.noreply.github.com>
2025-02-04 15:22:09 +0530

    Update uploading_files.py

--- dags/uploading_files.py
+++ dags/uploading_files.py
@@ -1,16 +1,17 @@
 from airflow import DAG
 from datetime import datetime
-from airflow.providers.amazon.aws.transfers.local_to_s3 import LocalFilesystemToS3Operator
+from airflow.providers.amazon.aws.transfers.local_to_s3 import S3FileTransferOperator  
 
 with DAG(
         'local_to_s3',
         schedule='@once',
         tags=['uploading'],
         description='Uploading a file from local system to S3',
-        start_date=datetime(2025, 4, 2)
+        start_date=datetime(2025, 4, 2),
+        catchup=False  # Avoid backfilling
         ) as dag:
     
-    upload = LocalFilesystemToS3Operator(
+    upload = S3FileTransferOperator(
         task_id='uploading_task',
         filename=r'C:\Users\kotha.rao\Desktop\Boot camp\IPL_Ball_by_Ball_2008_2022.csv',
         bucket_name='knrbucket',
