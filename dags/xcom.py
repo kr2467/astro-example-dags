5da81bbe00a0e6c684399f9dbcbe0eaff35c9616
kr2467 <104669142+kr2467@users.noreply.github.com>
2025-02-04 18:07:29 +0530

    Update xcom.py

--- dags/xcom.py
+++ dags/xcom.py
@@ -4,31 +4,32 @@ from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
 
 def push_value_to_xcom(**kwargs):
-    kwargs['ti'].xcom_push(key='my_number',value=42)
+    kwargs['ti'].xcom_push(key='my_number', value=42)
     
 def pull_value_from_xcom(**kwargs):
-    ti=kwargs['ti']
-    value=ti.xcom_pull(task_id='task_push_value',key='my_number')
-    print(f"pulled valued from xcom :{value}")
+    ti = kwargs['ti']
+    value = ti.xcom_pull(task_ids='push_value', key='my_number')
+    print(f"Pulled value from XCom: {value}")
     
 with DAG(
         'xcom_execution',
-        schedule='@once',
-        start=datetime(2025,4,2),
+        schedule_interval='@once',
+        start_date=datetime(2025, 4, 2),  # Corrected the argument name to `start_date`
         catchup=False
         ) as dag:
-    start=DummyOperator('start')
     
-    task_push_value=PythonOperator(
+    start = DummyOperator(task_id='start')
+    
+    task_push_value = PythonOperator(
         task_id='push_value',
         python_callable=push_value_to_xcom,
-        provide_context=True
-        )
-    task_pull_value=PythonOperator(
+    )
+    
+    task_pull_value = PythonOperator(
         task_id='pull_value',
         python_callable=pull_value_from_xcom,
-        provide_context=True
-        )
-    end=DummyOperator('end')
+    )
     
-    start>>task_push_value>>task_pull_value>>end
+    end = DummyOperator(task_id='end')
+    
+    start >> task_push_value >> task_pull_value >> end
