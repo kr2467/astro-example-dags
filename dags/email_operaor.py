692d5a244f9b3f4f2e13bdcaa056ede74d81d31a
kr2467 <104669142+kr2467@users.noreply.github.com>
2025-02-04 11:30:08 +0530

    Update email_operaor.py

--- dags/email_operaor.py
+++ dags/email_operaor.py
@@ -6,18 +6,16 @@ from airflow.operators.email import EmailOperator
 from datetime import datetime
 
 with DAG('s3_glue_dynamodb',
-         start_date=datetime(2025,1,30),
+         start_date=datetime(2025,4,2),
          description='Automation of s3 to dynamodb',
          tags=['Air flow'],
-         schedule='@daily',catchup=False) as dag:
-             
-             send_email=EmailOperator(
+         schedule='@daily', catchup=False) as dag:
+    
+    send_email = EmailOperator(
         task_id='send_email_on_success',
         to='kotharao007@gmail.com',
-        cc='projectmanager@gmail.com',
         subject='Glue job ran successfully and data is stored in the dynamo db',
-        html_content='The tranformation of the data has been completed and the data is stored in the dynamodb successfully',
-        dag=dag
-        )
-        
-        send_email
+        html_content='The transformation of the data has been completed and the data is stored in DynamoDB successfully'
+    )
+
+    send_email
