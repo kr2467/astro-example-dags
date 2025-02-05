f6264ff36c7ce8d8be6b29b6b556e4ed3f5419a9
kr2467 <104669142+kr2467@users.noreply.github.com>
2025-02-05 12:03:57 +0530

    Update set_variables.py

--- dags/set_variables.py
+++ dags/set_variables.py
@@ -1,4 +1,5 @@
+from airflow.models import Variable
+
 def set_var():
-    from airflow.models import Variable
-    Variable.set(key="my_regular_var",value="Hello!")
-    Variable.set(key="my_json_var",value={"num1":32,"num2":23},serialize_json=True)
+    Variable.set(key="my_regular_var", value="Hello!")
+    Variable.set(key="my_json_var", value={"num1": 32, "num2": 23}, serialize_json=True)
