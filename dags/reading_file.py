86d69c23a08b637552d4484c3146d57df47a8229
kr2467 <104669142+kr2467@users.noreply.github.com>
2025-02-05 16:09:42 +0530

    Update reading_file.py

--- dags/reading_file.py
+++ dags/reading_file.py
@@ -3,26 +3,43 @@ from datetime import datetime
 from airflow.operators.python import PythonOperator
 import boto3
 
+def read_file(bucket_name,file_key):
+    s3_client=boto3.client('s3')
+    response=s3_client.get_object(Bucket=bucket_name,Key=file_key)
+    file_content=response['Body'].read().decode('utf-8')
 
-def read_file(bucket_name, file_key):
-    s3_client = boto3.client('s3')
-    response = s3_client.get_object(Bucket=bucket_name, Key=file_key)
-    file_content = response['Body'].read().decode('utf-8')
-    print(file_content)
-
+def upload_back(file_content,bucket_name,file_key):
+    s3_client=boto3.client('s3')
+    new_file_key=f"processed/{file_key}"
+    s3_client.put_object(Bucket=bucket_name,Key=new_file_key,Body=file_content)
+    print("file uploaded to {bucket_name}/{new_file_key}")
 
 with DAG(
-        'reading_a_file',
-        schedule_interval=None,  
-        start_date=datetime(2025, 5, 2),
+        'retreving_a_file',
+        schedule='@once',
+        start_date=datetime(2025,5,2),
         catchup=False,
-) as dag:
-
-    # Define the task
-    reading_file = PythonOperator(
+        task=['reading a file']
+        ) as dag:
+    
+    source_bucket='knrbucket'
+    file_key="IPL_Ball_by_Ball_2008_2022.csv"
+    reading_file=PythonOperator(
         task_id='reading_file_from_s3',
         python_callable=read_file,
-        op_args=['knrbucket', 'IPL_Ball_by_Ball_2008_2022.csv'],  
-    )
-
-    reading_file  
+        op_args=[source_bucket,file_key],
+        provide_context=True
+        )
+    upload_file=PythonOperator(
+        task_id='uploading a file',
+        python_callable=upload_back,
+        op_args=[
+            '{{task_instance.xcom_pull(task_ids="read_file")}}',
+            source_bucket,
+            file_key
+            ]
+        
+        )
+    
+    
+    reading_file>>upload_file
