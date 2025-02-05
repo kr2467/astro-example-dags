50c675d9f1ddf26060b8427560fbcd2e158ba3be
kr2467 <104669142+kr2467@users.noreply.github.com>
2025-02-05 21:23:40 +0530

    Update readdddddddddddddddd.py

--- dags/readdddddddddddddddd.py
+++ dags/readdddddddddddddddd.py
@@ -4,7 +4,7 @@ from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from datetime import datetime
 
 def read_s3_file(**kwargs):
-    s3_hook = S3Hook(aws_conn_id='aws_s3_conn')
+    s3_hook = S3Hook(aws_conn_id='s3_conn')
     s3_key = 'IPL_Ball_by_Ball_2008_2022.csv'
     bucket_name = 'knrbucket'
     
