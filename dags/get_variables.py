cf0167f66e5a342d65493c0e812fb382d384200c
kr2467 <104669142+kr2467@users.noreply.github.com>
2025-02-05 11:53:34 +0530

    Update get_variables.py

--- dags/get_variables.py
+++ dags/get_variables.py
@@ -1,3 +1,3 @@
 from airflow.models import Variable
-my_var=variable.get("my_json_var")
-my_regular_var=variable.get("my_regular_var",deserialize_json=True)
+my_var = Variable.get("my_json_var")
+my_regular_var = Variable.get("my_regular_var", deserialize_json=True)
