725574cda79d90fffc6811461294dff02aca710e
kr2467 <104669142+kr2467@users.noreply.github.com>
2025-02-06 22:17:11 +0530

    Update file_sensor.py

--- dags/file_sensor.py
+++ dags/file_sensor.py
@@ -1,19 +1,20 @@
 from airflow import DAG
-from airflow.providers.amazon.aws.sensors.s3 import S3FileSensor
+from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
 from datetime import datetime
 
 with DAG('s3_glue_dynamodb',
-         start_date=datetime(2025,1,30),
+         start_date=datetime(2025, 1, 30),
          description='Automation of s3 to dynamodb',
          tags=['Air flow'],
-         schedule='@daily',catchup=False) as dag:
+         schedule_interval='@daily',
+         catchup=False) as dag:
     
-    s3_sensor=S3FileSensor(
+    s3_sensor = S3KeySensor(
         task_id='s3_file_sensor',
         bucket_name='triggerbucketobject',
+        bucket_key='path/to/your/file.txt',  # Specify the key of the file you're waiting for
         aws_conn_id='aws_default',
         poke_interval=60,
-        time_out=60,
+        timeout=60,
         dag=dag
-        )
-    
+    )
