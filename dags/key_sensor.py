fc946b5dfcd7ed16ded1decd576858aa39b9e61e
kr2467 <104669142+kr2467@users.noreply.github.com>
2025-02-12 15:09:13 +0530

    Update key_sensor.py

--- dags/key_sensor.py
+++ dags/key_sensor.py
@@ -1,24 +1,21 @@
 from airflow.providers.amazon.aws.sensors.s3_key import S3KeySensor
-from airflow.operators import DummyOperator
+from airflow.operators.dummy import DummyOperator
 from datetime import datetime
 from airflow import DAG
 
-
 with DAG(
         's3_key_sensors',
-        start_date=datetime(2025,2,10),
-        schedule='@once',
-        poke_interval=60,
-        description='Atrail of filesensor'
+        start_date=datetime(2025, 2, 10),
+        schedule_interval='@once',
+        description='A trail of file sensor'
         ) as dag:
-    s3_sensor=S3KeySensor(
+    s3_sensor = S3KeySensor(
         task_id='checking_file_existence',
         bucket_name='triggerbucketobject',
         bucket_key='IPL_Ball_by_Ball_2008_2022.csv',
         poke_interval=60
-        
-        )
-    end_task=DummyOperator(
-        'end_task'
-        )
-    s3_sensor>>end_task
+    )
+    end_task = DummyOperator(
+        task_id='end_task'
+    )
+    s3_sensor >> end_task
