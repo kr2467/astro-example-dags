4f495dd99010ac5958b50b658112001a62f59018
kr2467 <104669142+kr2467@users.noreply.github.com>
2025-02-12 14:59:56 +0530

    Create key_sensor.py

---
+++ dags/key_sensor.py
@@ -0,0 +1,24 @@
+from airflow.providers.amazon.aws.sensors.s3_key import S3KeySensor
+from airflow.operators import DummyOperator
+from datetime import datetime
+from airflow import DAG
+
+
+with DAG(
+        's3_key_sensor',
+        start_date=datetime(2025,2,10),
+        schedule='@once',
+        poke_interval=60,
+        description='Atrail of filesensor'
+        ) as dag:
+    s3_sensor=S3KeySensor(
+        task_id='checking_file_existence',
+        bucket_name='triggerbucketobject',
+        bucket_key='IPL_Ball_by_Ball_2008_2022.csv',
+        poke_interval=60
+        
+        )
+    end_task=DummyOperator(
+        'end_task'
+        )
+    s3_sensor>>end_task
